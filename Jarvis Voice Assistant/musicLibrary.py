977854b9a0dc8d0693fca036fe75a5b07d280a75
Kushagra <kushagraagrawal128@gmail.com>
2024-12-14 23:39:57 +0530

    Delete Jarvis Voice Assistant directory

--- Jarvis Voice Assistant/musicLibrary.py
+++
@@ -1,4 +0,0 @@
-music = {"stealth":"https://www.youtube.com/watch?v=U47Tr9BB_wE",
-         "khwab":"https://www.youtube.com/watch?v=dBlOIo809ds",
-         "thodi der":"https://www.youtube.com/watch?v=dBlOIo809ds",
-         "glow":"https://www.youtube.com/watch?v=CU9gwkZcJgs&pp=ygUJZ2xvdyBzb25n"}
